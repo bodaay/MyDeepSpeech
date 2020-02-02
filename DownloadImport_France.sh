384fd64c81cc1235efbeaed7e0439f5c2aae57d1
Khalefa Al-Ahmad <khalefa@al-ahmad.com>
2020-02-02 11:50:17 +0300

    first commit

---
+++ DownloadImport_France.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+language="fr"
+tarFileName="$language.tar.gz"
+DownloadBaseLink="https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10"
+mkdir -p /deepspeech/data/CV
+wget -O /deepspeech/data/CV/$tarFileName $DownloadBaseLink/$tarFileName
+cd /deepspeech/data/CV/
+tar xzvf $tarFileName
+cd /deepspeech/DeepSpeech
+bin/import_cv2.py /deepspeech/data/CV/$language/
+
+
