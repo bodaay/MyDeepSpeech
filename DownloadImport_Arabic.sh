551c85bb0dd4511d2b75c3a36bd3f7c88f700d33
bodaay <bodaay@users.noreply.github.com>
2020-02-03 09:16:23 +0300

    Update DownloadImport_Arabic.sh

--- DownloadImport_Arabic.sh
+++ DownloadImport_Arabic.sh
@@ -4,7 +4,7 @@ tarFileName="$language.tar.gz"
 path="/data"
 DownloadBaseLink="https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10"
 mkdir -p $path/CV/$language
-wget -O $path/CV/$language/$tarFileName $DownloadBaseLink/$tarFileName
+wget -O $path/CV/$language/$tarFileName -r -N -c $DownloadBaseLink/$tarFileName
 cd $path/CV/$language
 tar xzvf $tarFileName
 cd /DeepSpeech
