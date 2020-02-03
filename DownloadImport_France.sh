f177b5e58f77bf90ea7254d9826c02d1eb9e1830
bodaay <bodaay@users.noreply.github.com>
2020-02-03 07:17:06 +0300

    Update DownloadImport_France.sh

--- DownloadImport_France.sh
+++ DownloadImport_France.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 language="fr"
 tarFileName="$language.tar.gz"
+path="/data"
 DownloadBaseLink="https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-4-2019-12-10"
-mkdir -p /deepspeech/data/CV/$language
-wget -O /deepspeech/data/CV/$language/$tarFileName $DownloadBaseLink/$tarFileName
-cd /deepspeech/data/CV/$language
+mkdir -p $path/CV/$language
+wget -O $path/CV/$language/$tarFileName $DownloadBaseLink/$tarFileName
+cd $path/CV/$language
 tar xzvf $tarFileName
-cd /deepspeech/DeepSpeech
-bin/import_cv2.py /deepspeech/data/CV/$language/
+cd /DeepSpeech
+bin/import_cv2.py $path/CV/$language/
 
 
