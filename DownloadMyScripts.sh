c4c3a9b5555efd1d50ba20d7ce5c5fd9920c953f
bodaay <bodaay@users.noreply.github.com>
2020-02-03 08:57:18 +0300

    Create DownloadMyScripts.sh

---
+++ DownloadMyScripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -O DownloadImport_Arabic.sh https://raw.githubusercontent.com/bodaay/MyDeepSpeech/master/DownloadImport_Arabic.sh
+wget -O DownloadImport_Germany.sh https://raw.githubusercontent.com/bodaay/MyDeepSpeech/master/DownloadImport_Germany.sh
+wget -O DownloadImport_France.sh https://raw.githubusercontent.com/bodaay/MyDeepSpeech/master/DownloadImport_France.sh
+wget -O Train_Arabic.sh https://raw.githubusercontent.com/bodaay/MyDeepSpeech/master/Train_Arabic.sh
+chmod +x Train_*
+chmod +x DownloadImport*
